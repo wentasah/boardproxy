81e44409b4884c403b53d449f69f17169ed52f6f
Michal Sojka <michal.sojka@cvut.cz>
2020-11-04 13:47:18 +0100

    Add all our current boards

--- boards.cpp
+++ boards.cpp
@@ -6,5 +6,16 @@ std::list<Board> boards = {
   { "bash -c 'echo Welcome to board 1; sleep 3'", "127.0.0.1" },
   { "bash -c 'echo Welcome to board 2; sleep 3'", "127.0.0.1" },
 #else
+#define CMD "ssh psr-hw@c2c1 sterm -b 2000 -s 115200 /dev/serial/by-id/"
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00EM7L-if00-port0", "10.35.95.50" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00ENK1-if00-port0", "10.35.95.51" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00EO5X-if00-port0", "10.35.95.52" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00EQSO-if00-port0", "10.35.95.53" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00EREM-if00-port0", "10.35.95.54" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00ERUW-if00-port0", "10.35.95.55" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DJ00F380-if00-port0", "10.35.95.56" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DN00JRK2-if00-port0", "10.35.95.57" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DN00JU72-if00-port0", "10.35.95.58" },
+  { CMD "usb-FTDI_FT230X_Basic_UART_DN00JU7G-if00-port0", "10.35.95.59" },
 #endif
 };
