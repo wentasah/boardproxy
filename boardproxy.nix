f455e86748a9d4983f1e0c6448a65a3f795c5b95
Michal Sojka <michal.sojka@cvut.cz>
2021-02-08 22:54:51 +0100

    nix: Use pkgs.lib rather than deprecated stdenv.lib

--- boardproxy.nix
+++ boardproxy.nix
@@ -1,4 +1,4 @@
-{ stdenv, meson, ninja, pkg-config, libev, fmt, spdlog, boost, nix-gitignore, systemd }:
+{ stdenv, lib, meson, ninja, pkg-config, libev, fmt, spdlog, boost, nix-gitignore, systemd }:
 stdenv.mkDerivation {
   name = "boardproxy";
   src = nix-gitignore.gitignoreSource [] ./.;
@@ -7,12 +7,12 @@ stdenv.mkDerivation {
 
   # Meson is no longer able to pick up Boost automatically.
   # https://github.com/NixOS/nixpkgs/issues/86131
-  BOOST_INCLUDEDIR = "${stdenv.lib.getDev boost}/include";
-  BOOST_LIBRARYDIR = "${stdenv.lib.getLib boost}/lib";
+  BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
+  BOOST_LIBRARYDIR = "${lib.getLib boost}/lib";
 
   # Export the same also to the shell started via shell.nix
   shellHook = ''
-    export BOOST_INCLUDEDIR="${stdenv.lib.getDev boost}/include"
-    export BOOST_LIBRARYDIR="${stdenv.lib.getLib boost}/lib"
+    export BOOST_INCLUDEDIR="${lib.getDev boost}/include"
+    export BOOST_LIBRARYDIR="${lib.getLib boost}/lib"
   '';
 }
