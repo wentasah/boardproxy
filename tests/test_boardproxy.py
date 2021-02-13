e56e2df9c2a6659f02ca05179e2a3730b2ec234a
Michal Sojka <michal.sojka@cvut.cz>
2021-02-13 16:40:08 +0100

    tests: Run each test in separate directory + some debugging aid

--- tests/test_boardproxy.py
+++ tests/test_boardproxy.py
@@ -2,6 +2,8 @@ import subprocess
 from subprocess import Popen, PIPE
 import shlex
 import os
+from datetime import datetime
+import pytest
 
 # d = Popen(['ls', '-l'], stdout=PIPE, text=True)
 # for line in d.stdout:
@@ -21,6 +23,7 @@ class Daemon(object):
     def __init__(self, config, await_listening=True, assert_returncode=True):
         with open('config.toml', 'w') as cfg:
             cfg.write(config)
+        # strace = ['strace', '-f', '-DD', '--timestamps=time,ms', '-o', 'strace.log']
         self.process = Popen(['boardproxy'] +
                              ['--daemon', '--config=config.toml', '.'],
                              stdout=PIPE, stderr=PIPE, text=True)
@@ -48,10 +51,15 @@ def run(args):
     return subprocess.run(args, capture_output=True, text=True)
 
 
+@pytest.fixture(autouse=True)
+def run_in_tmp_path(tmp_path):
+    print("Changing directory to", tmp_path)
+    os.chdir(tmp_path)
+
+
 #######
 # Tests
 #######
-
 
 def test_no_sock_dir():
     res = run('boardproxy --daemon')
@@ -63,6 +71,7 @@ def test_missing_ip_address():
     config = '''\
 [boards.1]
 command = "echo This is board 1"
+# ip_address missing
 '''
     with Daemon(config, await_listening=False, assert_returncode=False) as daemon:
         (stdout, stderr) = daemon.communicate()
@@ -85,17 +94,17 @@ ip_address = "127.0.0.1"
 def test_disconnect():
     config = '''\
 [boards.1]
-command = "echo This is board 1"
+command = "bash -c 'echo This is board 1; read -t2; echo Done'"
 ip_address = "127.0.0.1"
 '''
     with Daemon(config):
         client = Popen(['boardproxy', '.'], stdout=PIPE, text=True)
         board_ok = False
         for line in client.stdout:
+            print(line)
             if line == "This is board 1\n":
                 board_ok = True
                 break
-            print(line)
         client.terminate()  # Disconnect after first line
         client.wait()
         assert client.returncode == -15
@@ -125,6 +134,8 @@ board2 = { name = "B", ip_address = "127.0.0.1" }
 '''
     with Daemon(config):
         client1 = Popen(['boardproxy', '.'], stdout=PIPE, stderr=PIPE, text=True)
+        # Reading below also ensures that next client is started after
+        # the first is connected
         assert client1.stderr.readline() == "Connecting to board board1\n"
         assert client1.stdout.readline() == "This is board A\n"
 
@@ -138,6 +149,6 @@ board2 = { name = "B", ip_address = "127.0.0.1" }
         assert "waiting" in client3.stderr.readline()
         assert client3.stderr.readline() == "No board currently available. Waiting...\n"
 
-        for c in [client1, client2, client3]:
-            c.terminate()
-            c.wait()
+        for client in [client1, client2, client3]:
+            client.terminate()
+            client.wait()
